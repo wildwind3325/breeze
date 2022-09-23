d801c888618c28c7254b6dd6d47279684da9a9c3
张俊 <zhangjun01@hesaitech.com>
2022-09-23 19:19:26 +0800

    data picker

---
+++ desktop/src/components/DataPicker.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-cascader :options="list" clearable :show-all-levels="false" />
+</template>
+
+<script>
+export default {
+  name: 'DataPicker',
+  props: {
+    code: String,
+    depth: Number,
+    props: Object
+  },
+  data() {
+    return {
+      list: []
+    };
+  },
+  mounted() {
+  }
+}
+</script>
